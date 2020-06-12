e32a9dadf294319befe8493409fcf65561fd47cf
Dandandan <danielheres@gmail.com>
2020-06-12 08:51:32 +0200

    Formatting

--- src/parser.rs
+++ src/parser.rs
@@ -1006,7 +1006,11 @@ impl Parser {
         let table_name = self.parse_object_name()?;
         let (columns, constraints) = self.parse_columns()?;
         self.expect_keywords(&[Keyword::STORED, Keyword::AS])?;
-        let file_format = self.parse_identifier()?.value.to_ascii_uppercase().parse::<FileFormat>()?;
+        let file_format = self
+            .parse_identifier()?
+            .value
+            .to_ascii_uppercase()
+            .parse::<FileFormat>()?;
 
         self.expect_keyword(Keyword::LOCATION)?;
         let location = self.parse_literal_string()?;

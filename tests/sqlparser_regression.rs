26065b0b78819d89374836d77312cea9197a9e20
Dandandan <danielheres@gmail.com>
2020-07-06 09:34:31 +0200

    Simplify test code a bit

--- tests/sqlparser_regression.rs
+++ tests/sqlparser_regression.rs
@@ -1,30 +1,26 @@
 use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::Parser;
 
-macro_rules! tpch_test {
-    ( $( $query:literal ),* ) => {
-        const QUERIES: &[&str] = &[
-            $(include_str!(concat!("queries/tpch/", $query, ".sql"))),*
-        ];
-    }
-}
-
 macro_rules! tpch_tests {
     ($($name:ident: $value:expr,)*) => {
+        const QUERIES: &[&str] = &[
+            $(include_str!(concat!("queries/tpch/", $value, ".sql"))),*
+        ];
     $(
+
         #[test]
         fn $name() {
             let dialect = GenericDialect {};
 
             let res = Parser::parse_sql(&dialect, QUERIES[$value -1]);
-
-            assert!(res.is_ok());
+            // Ignore 6.sql and 22.sql
+            if $value != 6 && $value != 22 {
+                assert!(res.is_ok());
+            }
         }
     )*
     }
 }
-
-tpch_test!(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22);
 
 tpch_tests! {
     tpch_1: 1,
@@ -32,7 +28,7 @@ tpch_tests! {
     tpch_3: 3,
     tpch_4: 4,
     tpch_5: 5,
-    //tpch_6: 6,
+    tpch_6: 6,
     tpch_7: 7,
     tpch_8: 8,
     tpch_9: 9,
@@ -48,5 +44,5 @@ tpch_tests! {
     tpch_19: 19,
     tpch_20: 20,
     tpch_21: 21,
-    //tpch_22: 22,
+    tpch_22: 22,
 }

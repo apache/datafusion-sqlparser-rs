c56cfccf9d705014395c196102b3744f444a8207
Stephen Carman <shcarman@gmail.com>
2020-07-28 11:13:29 -0400

    Support for columns defined after partition

--- src/parser.rs
+++ src/parser.rs
@@ -2443,12 +2443,17 @@ impl Parser {
         } else {
             None
         };
+
+        // Hive allows you to specify columns after partitions as well if you want.
+        let after_columns = self.parse_parenthesized_column_list(Optional)?;
+
         let source = Box::new(self.parse_query()?);
         Ok(Statement::Insert {
             table_name,
             overwrite,
             partitioned,
             columns,
+            after_columns,
             source,
             table,
         })

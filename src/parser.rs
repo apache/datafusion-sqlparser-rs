27a91dec6ec75278a1340c5af7f2322efabbb0ab
Stephen Carman <shcarman@gmail.com>
2020-07-31 17:24:40 -0400

    Allow ommitting add/drop/sync partitions in msck

--- src/parser.rs
+++ src/parser.rs
@@ -174,14 +174,22 @@ impl<'a> Parser<'a> {
         let repair = self.parse_keyword(Keyword::REPAIR);
         self.expect_keyword(Keyword::TABLE)?;
         let table_name = self.parse_object_name()?;
-        let partition_action =
-            match self.parse_one_of_keywords(&[Keyword::ADD, Keyword::DROP, Keyword::SYNC]) {
-                Some(Keyword::ADD) => Some(AddDropSync::ADD),
-                Some(Keyword::DROP) => Some(AddDropSync::DROP),
-                Some(Keyword::SYNC) => Some(AddDropSync::SYNC),
-                _ => None,
-            };
-        self.expect_keyword(Keyword::PARTITIONS)?;
+        let partition_action = self
+            .maybe_parse(|parser| {
+                let pa = match parser.parse_one_of_keywords(&[
+                    Keyword::ADD,
+                    Keyword::DROP,
+                    Keyword::SYNC,
+                ]) {
+                    Some(Keyword::ADD) => Some(AddDropSync::ADD),
+                    Some(Keyword::DROP) => Some(AddDropSync::DROP),
+                    Some(Keyword::SYNC) => Some(AddDropSync::SYNC),
+                    _ => None,
+                };
+                parser.expect_keyword(Keyword::PARTITIONS)?;
+                Ok(pa)
+            })
+            .unwrap_or_default();
         Ok(Statement::Msck {
             repair,
             table_name,

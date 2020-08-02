db4bad0a2d6a50a3733cecdbe215dc85fd339091
Eyal Leshem <eyal@satoricyber.com>
2020-08-02 17:30:47 +0300

    Add snowflake dialect

---
+++ tests/sqlparser_snowflake.rs
@@ -0,0 +1,31 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+use sqlparser::ast::*;
+use sqlparser::dialect::{GenericDialect, SnowflakeDialect};
+use sqlparser::test_utils::*;
+
+#[test]
+fn test_snowflake_create_table() {
+    let sql = "CREATE TABLE _my_$table (am00unt number)";
+    match snowflake_and_generic().verified_stmt(sql) {
+        Statement::CreateTable { name, .. } => {
+            assert_eq!("_my_$table", name.to_string());
+        }
+        _ => unreachable!(),
+    }
+}
+
+fn snowflake_and_generic() -> TestedDialects {
+    TestedDialects {
+        dialects: vec![Box::new(SnowflakeDialect {}), Box::new(GenericDialect {})],
+    }
+}

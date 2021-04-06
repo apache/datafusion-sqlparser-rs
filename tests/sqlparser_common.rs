c4add38d93145b39991b6a40f55dca12223ca950
joshwd36 <joshwd36@gmail.com>
2021-04-06 21:08:58 +0100

    Remove copy/paste error

--- tests/sqlparser_common.rs
+++ tests/sqlparser_common.rs
@@ -1,4 +1,4 @@
-// Licensed under the Apache License, Version 2. name: (), columns: (), f columns: (), foreign_table: (), referred_columns: (), on_delete: (), on_update: ()oreign_table: (), referred_columns: (), on_delete: (), on_update: () name: (), columns: (), foreign_table: (), referred_columns: (), on_delete: (), on_update: ()0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //

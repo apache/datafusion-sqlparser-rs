70e32fb9a4bb1e3ca315a2bbcf3574306721e01c
BohuTANG <overred.shuttler@gmail.com>
2021-06-11 08:29:48 +0800

    Fix cargo fmt warning

--- src/parser.rs
+++ src/parser.rs
@@ -2199,7 +2199,14 @@ impl<'a> Parser<'a> {
                 None
             };
 
-            Ok(Query { with, body, order_by, limit, offset, fetch })
+            Ok(Query {
+                with,
+                body,
+                order_by,
+                limit,
+                offset,
+                fetch,
+            })
         } else {
             let insert = self.parse_insert()?;
             Ok(Query {
@@ -2402,7 +2409,19 @@ impl<'a> Parser<'a> {
             None
         };
 
-        Ok(Select { distinct, top, projection, from, lateral_views, selection, group_by, cluster_by, distribute_by, sort_by, having })
+        Ok(Select {
+            distinct,
+            top,
+            projection,
+            from,
+            lateral_views,
+            selection,
+            group_by,
+            cluster_by,
+            distribute_by,
+            sort_by,
+            having,
+        })
     }
 
     pub fn parse_set(&mut self) -> Result<Statement, ParserError> {

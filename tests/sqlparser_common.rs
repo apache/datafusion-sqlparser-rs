a1dce106034e95e8fc68d8f5dde14297548d85b0
Matthew Turner <matthew.m.turner@outlook.com>
2022-03-31 20:16:55 -0400

    Fix CreateDatabse parsing

--- tests/sqlparser_common.rs
+++ tests/sqlparser_common.rs
@@ -3406,6 +3406,25 @@ fn parse_create_database() {
 }
 
 #[test]
+fn parse_create_database_ine() {
+    let sql = "CREATE DATABASE IF NOT EXISTS mydb";
+    match verified_stmt(sql) {
+        Statement::CreateDatabase {
+            db_name,
+            if_not_exists,
+            location,
+            managed_location,
+        } => {
+            assert_eq!("mydb", db_name.to_string());
+            assert_eq!(true, if_not_exists);
+            assert_eq!(None, location);
+            assert_eq!(None, managed_location);
+        }
+        _ => unreachable!(),
+    }
+}
+
+#[test]
 fn parse_create_view() {
     let sql = "CREATE VIEW myschema.myview AS SELECT foo FROM bar";
     match verified_stmt(sql) {

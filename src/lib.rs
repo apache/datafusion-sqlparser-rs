8346e05e55583e56a81999e33d7e0ac062ecb29e
Jon Mease <jon.mease.dev@gmail.com>
2022-07-29 11:25:40 -0400

    Fix docs example

--- src/lib.rs
+++ src/lib.rs
@@ -18,17 +18,14 @@
 //! into an Abstract Syntax Tree (AST).
 //!
 //! ```
-//! use sqlparser::dialect::GenericDialect;
 //! use sqlparser::parser::Parser;
-//!
-//! let dialect = GenericDialect {}; // or AnsiDialect
 //!
 //! let sql = "SELECT a, b, 123, myfunc(b) \
 //!            FROM table_1 \
 //!            WHERE a > b AND b < 100 \
 //!            ORDER BY a DESC, b";
 //!
-//! let ast = Parser::parse_sql(&dialect, sql).unwrap();
+//! let ast = Parser::parse_sql_query(sql).unwrap();
 //!
 //! println!("AST: {:?}", ast);
 //! ```

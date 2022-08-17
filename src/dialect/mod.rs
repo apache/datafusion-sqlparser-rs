3c6528463a59d66f1b3e78f363bee806c5af0cc3
Andy Grove <andygrove73@gmail.com>
2022-08-17 13:58:33 -0600

    no-std support

--- src/dialect/mod.rs
+++ src/dialect/mod.rs
@@ -10,6 +10,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[cfg(not(feature = "std"))]
+use alloc::boxed::Box;
+
 mod ansi;
 mod bigquery;
 mod clickhouse;

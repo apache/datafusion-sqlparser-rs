97b4059373e7d9db34b7a46c36be54f003a0d9a9
sam-mmm <2740878+sam-mmm@users.noreply.github.com>
2022-10-21 01:23:00 +0530

    Fix for format! not avalable in --target thumbv6m-none-eabi

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -2489,7 +2489,7 @@ impl fmt::Display for Statement {
                 let as_type: String = if let Some(dt) = data_type.as_ref() {
                     //Cannot use format!(" AS {}", dt), due to format! is not available in --target thumbv6m-none-eabi
                     // " AS ".to_owned() + &dt.to_string()
-                    [" AS ",&dt.to_string()].concat()
+                    [" AS ", &dt.to_string()].concat()
                 } else {
                     "".to_string()
                 };

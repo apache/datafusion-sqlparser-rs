00d27d04a0164b4740cd56e346329526dbeaa479
Yuval Shkolar <yuval@illumex.ai>
2022-11-02 10:27:32 +0200

    Add support in key location

--- src/parser.rs
+++ src/parser.rs
@@ -3448,7 +3448,8 @@ impl<'a> Parser<'a> {
                     Some('\'') => Ok(Value::SingleQuotedString(w.value)),
                     _ => self.expected("A value?", Token::Word(w))?,
                 },
-                Keyword::NoKeyword if dialect_of!(self is SnowflakeDialect | GenericDialect) => {
+                // Case when Snowflake Semi-structured data like key:value
+                Keyword::NoKeyword | Keyword::LOCATION if dialect_of!(self is SnowflakeDialect | GenericDialect) => {
                     Ok(Value::UnQuotedString(w.value))
                 }
                 _ => self.expected("a concrete value", Token::Word(w)),

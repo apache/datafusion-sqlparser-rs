43ee8cdc017ecfb1d5b1f4f3d323cc56b74faf60
step-baby <3528004749@qq.com>
2022-11-28 11:34:57 +0800

    fix: timezone

--- src/parser.rs
+++ src/parser.rs
@@ -4553,7 +4553,10 @@ impl<'a> Parser<'a> {
                 charset_name,
                 collation_name,
             })
-        } else if self.consume_token(&Token::Eq) || self.parse_keyword(Keyword::TO) {
+        } else if self.consume_token(&Token::Eq)
+            || self.parse_keyword(Keyword::TO)
+            || variable == ObjectName(vec!["TIMEZONE".into()])
+        {
             let mut values = vec![];
             loop {
                 let value = if let Ok(expr) = self.parse_expr() {

d69b875367385cfc1d18296f390674cfc3729e4a
Ankur Goyal <agoyal@figma.com>
2023-03-06 06:55:55 -0800

    ClickHouse CREATE TABLE Fixes: add ORDER BY and fix clause ordering (#824)
    * Fix ClickHouse (add ORDER BY)
    
    * Improve test case

--- tests/sqlparser_clickhouse.rs
+++ tests/sqlparser_clickhouse.rs
@@ -317,6 +317,18 @@ fn parse_similar_to() {
     chk(true);
 }
 
+#[test]
+fn parse_create_table() {
+    clickhouse().verified_stmt(r#"CREATE TABLE "x" ("a" "int") ENGINE=MergeTree ORDER BY ("x")"#);
+    clickhouse().one_statement_parses_to(
+        r#"CREATE TABLE "x" ("a" "int") ENGINE=MergeTree ORDER BY "x""#,
+        r#"CREATE TABLE "x" ("a" "int") ENGINE=MergeTree ORDER BY ("x")"#,
+    );
+    clickhouse().verified_stmt(
+        r#"CREATE TABLE "x" ("a" "int") ENGINE=MergeTree ORDER BY ("x") AS SELECT * FROM "t" WHERE true"#,
+    );
+}
+
 fn clickhouse() -> TestedDialects {
     TestedDialects {
         dialects: vec![Box::new(ClickHouseDialect {})],

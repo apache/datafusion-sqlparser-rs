a149c8acb8e3e7b5a62f78d5af2b1abce03fc005
Maximilian Roos <m@maxroos.com>
2023-05-09 18:10:32 -0700

    use Box from alloc

--- src/dialect/mysql.rs
+++ src/dialect/mysql.rs
@@ -10,6 +10,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[cfg(not(feature = "std"))]
+use alloc::boxed::Box;
+
 use crate::{
     ast::{BinaryOperator, Expr},
     dialect::Dialect,

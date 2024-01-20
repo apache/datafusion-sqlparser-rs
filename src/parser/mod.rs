2c09b5532ab150ba2328289466a9890e48322c83
Quinn Sinclair <me@partiallytyped.dev>
2024-01-20 19:32:12 +0100

    Fixes `1103` - disallow dangling NO in create sequence options
    Fixes a bug where a `NO` meant for `NO CACHE` in create options
    would be consumed without being followed by `CACHE`.
    
    Fixes: `#1103`

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -8602,13 +8602,12 @@ impl<'a> Parser<'a> {
             )));
         }
         // [ [ NO ] CYCLE ]
-        if self.parse_keywords(&[Keyword::NO]) {
-            if self.parse_keywords(&[Keyword::CYCLE]) {
-                sequence_options.push(SequenceOptions::Cycle(true));
-            }
+        if self.parse_keywords(&[Keyword::NO, Keyword::CYCLE]) {
+            sequence_options.push(SequenceOptions::Cycle(true));
         } else if self.parse_keywords(&[Keyword::CYCLE]) {
             sequence_options.push(SequenceOptions::Cycle(false));
         }
+
         Ok(sequence_options)
     }
 

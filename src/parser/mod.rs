c864198924af01e0dca70f6e64ee7a7e71f5b336
universalmind303 <cory.grinstead@gmail.com>
2024-02-07 15:00:07 -0600

    fix spacing

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -8767,15 +8767,15 @@ impl<'a> Parser<'a> {
             })
         }
     }
-    // INSTALL [extension_name]
+    
+    /// INSTALL [extension_name]
     pub fn parse_install(&mut self) -> Result<Statement, ParserError> {
         let extension_name = self.parse_identifier(false)?;
 
         Ok(Statement::Install { extension_name })
     }
 
-    // LOAD [extension_name]
-
+    /// LOAD [extension_name]
     pub fn parse_load(&mut self) -> Result<Statement, ParserError> {
         let extension_name = self.parse_identifier(false)?;
         Ok(Statement::Load { extension_name })

f2d4f91273d1644b51f83dae0f1954ab70267026
lovasoa <pere.jobs@gmail.com>
2024-02-14 13:57:42 +0100

    accept JSON_TABLE both as an unquoted table name and a table-valued function
    see https://github.com/sqlparser-rs/sqlparser-rs/pull/1123#issuecomment-1943607093

--- tests/sqlparser_postgres.rs
+++ tests/sqlparser_postgres.rs
@@ -2329,6 +2329,26 @@ fn test_json() {
 }
 
 #[test]
+fn parse_json_table_is_not_reserved() {
+    // JSON_TABLE is not a reserved keyword in PostgreSQL, even though it is in SQL:2023
+    // see: https://en.wikipedia.org/wiki/List_of_SQL_reserved_words
+    match pg().verified_only_select("SELECT * FROM JSON_TABLE") {
+        Select { from, .. } => {
+            assert_eq!(1, from.len());
+            match &from[0].relation {
+                TableFactor::Table {
+                    name: ObjectName(name),
+                    ..
+                } => {
+                    assert_eq!("JSON_TABLE", name[0].value);
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+}
+
+#[test]
 fn test_composite_value() {
     let sql = "SELECT (on_hand.item).name FROM on_hand WHERE (on_hand.item).price > 9";
     let select = pg().verified_only_select(sql);

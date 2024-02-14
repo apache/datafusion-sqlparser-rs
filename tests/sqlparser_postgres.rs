37e6b8100bc7afc3a6f96526ea46a145f8c6bc9f
lovasoa <pere.jobs@gmail.com>
2024-02-14 14:05:33 +0100

    better error message

--- tests/sqlparser_postgres.rs
+++ tests/sqlparser_postgres.rs
@@ -2339,7 +2339,7 @@ fn parse_json_table_is_not_reserved() {
             name: ObjectName(name),
             ..
         } => assert_eq!("JSON_TABLE", name[0].value),
-        _ => unreachable!(),
+        _ => panic!("Expected JSON_TABLE to be parsed as a table name"),
     }
 }
 

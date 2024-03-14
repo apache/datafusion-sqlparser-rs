62d25939e0dbbf251aa43a0197b3a33f6894e4a3
ifeanyi <ifeanyi@validio.io>
2024-03-14 10:33:58 +0100

    BigQuery: support unquoted hyphen in table/view declaration
    Adds support for proper object name parsing in bigquery
    to within a `CREATE TABLE` or `CREATE VIEW` statement. Similar
    to the parsing logic for when the table/view is later referenced.
    
    The lacking part was that bigquery allows unquoted hyphens in the
    first part of an object name - so that e.g.
    `CREATE TABLE my-project.mydataset.mytable`
    wasn't being parsed correctly.

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3527,7 +3527,8 @@ impl<'a> Parser<'a> {
             && self.parse_keywords(&[Keyword::IF, Keyword::NOT, Keyword::EXISTS]);
         // Many dialects support `OR ALTER` right after `CREATE`, but we don't (yet).
         // ANSI SQL and Postgres support RECURSIVE here, but we don't support it either.
-        let name = self.parse_object_name(false)?;
+        let allow_unquoted_hyphen = dialect_of!(self is BigQueryDialect);
+        let name = self.parse_object_name(allow_unquoted_hyphen)?;
         let columns = self.parse_view_columns()?;
         let mut options = CreateTableOptions::None;
         let with_options = self.parse_options(Keyword::WITH)?;
@@ -4482,8 +4483,9 @@ impl<'a> Parser<'a> {
         global: Option<bool>,
         transient: bool,
     ) -> Result<Statement, ParserError> {
+        let allow_unquoted_hyphen = dialect_of!(self is BigQueryDialect);
         let if_not_exists = self.parse_keywords(&[Keyword::IF, Keyword::NOT, Keyword::EXISTS]);
-        let table_name = self.parse_object_name(false)?;
+        let table_name = self.parse_object_name(allow_unquoted_hyphen)?;
 
         // Clickhouse has `ON CLUSTER 'cluster'` syntax for DDLs
         let on_cluster = if self.parse_keywords(&[Keyword::ON, Keyword::CLUSTER]) {
@@ -4498,13 +4500,13 @@ impl<'a> Parser<'a> {
         };
 
         let like = if self.parse_keyword(Keyword::LIKE) || self.parse_keyword(Keyword::ILIKE) {
-            self.parse_object_name(false).ok()
+            self.parse_object_name(allow_unquoted_hyphen).ok()
         } else {
             None
         };
 
         let clone = if self.parse_keyword(Keyword::CLONE) {
-            self.parse_object_name(false).ok()
+            self.parse_object_name(allow_unquoted_hyphen).ok()
         } else {
             None
         };

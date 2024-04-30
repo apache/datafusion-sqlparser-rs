bafaa914b0d61a77e0ef012990868da4c7e944a5
Duong Cong Toai <35887761+duongcongtoai@users.noreply.github.com>
2024-04-30 23:16:23 +0200

    Support Struct datatype parsing for GenericDialect (#1241)

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -6638,7 +6638,7 @@ impl<'a> Parser<'a> {
                         ))))
                     }
                 }
-                Keyword::STRUCT if dialect_of!(self is BigQueryDialect) => {
+                Keyword::STRUCT if dialect_of!(self is BigQueryDialect | GenericDialect) => {
                     self.prev_token();
                     let (field_defs, _trailing_bracket) =
                         self.parse_struct_type_def(Self::parse_big_query_struct_field_def)?;

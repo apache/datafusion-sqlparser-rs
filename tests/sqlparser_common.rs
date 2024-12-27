0ed3a4a3dc946dc3c3ea00d277c2afa55c3069a6
Jax Liu <liugs963@gmail.com>
2024-12-27 13:54:38 +0800

    fix clippy

--- tests/sqlparser_common.rs
+++ tests/sqlparser_common.rs
@@ -3064,7 +3064,7 @@ fn gen_sign_number_case(value: &str, op: UnaryOperator) -> (Vec<String>, Vec<Sel
 /// - "-0.0"
 fn gen_number_case_with_sign(number: &str) -> (Vec<String>, Vec<SelectItem>) {
     let (mut input, mut expected) = gen_number_case(number);
-    for op in vec![UnaryOperator::Plus, UnaryOperator::Minus] {
+    for op in [UnaryOperator::Plus, UnaryOperator::Minus] {
         let (input_sign, expected_sign) = gen_sign_number_case(number, op);
         input.extend(input_sign);
         expected.extend(expected_sign);

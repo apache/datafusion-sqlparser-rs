98a5627acdb172f0ec4c2ee195ecbceee6fd6a08
Yoav Cohen <yoav@satoricyber.com>
2025-01-01 20:42:33 +0100

    Add support for Snowflake column aliases that use SQL keywords

--- src/dialect/mod.rs
+++ src/dialect/mod.rs
@@ -782,6 +782,13 @@ pub trait Dialect: Debug + Any {
     fn supports_insert_set(&self) -> bool {
         false
     }
+
+    /// Returns true if the specified keyword should be parsed as a select item alias.
+    /// When explicit is true, the keyword is preceded by an `AS` word. Parser is provided
+    /// to enable looking ahead if needed.
+    fn is_select_item_alias(&self, explicit: bool, kw: &Keyword, _parser: &mut Parser) -> bool {
+        explicit || !keywords::RESERVED_FOR_COLUMN_ALIAS.contains(kw)
+    }
 }
 
 /// This represents the operators for which precedence must be defined

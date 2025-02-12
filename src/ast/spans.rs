a6a8f076e7683b9f20bc54eb2aceb006af675541
PokIsemaine <851892190@qq.com>
2025-02-12 20:10:07 +0800

    fix: missing spans OrderBy::All

--- src/ast/spans.rs
+++ src/ast/spans.rs
@@ -1099,7 +1099,7 @@ impl Spanned for ProjectionSelect {
 /// # partial span
 ///
 /// Missing spans:
-/// - [OrderByExpr::All]
+/// - [OrderBy::All]
 impl Spanned for OrderBy {
     fn span(&self) -> Span {
         match self {

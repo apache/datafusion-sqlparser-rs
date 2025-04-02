13bd374b86b10368269181ed5bbb5fc9efadf814
tomershaniii <65544633+tomershaniii@users.noreply.github.com>
2025-04-02 14:13:12 +0300

    Update src/dialect/mod.rs
    Co-authored-by: Ifeanyi Ubah <ify1992@yahoo.com>

--- src/dialect/mod.rs
+++ src/dialect/mod.rs
@@ -890,7 +890,7 @@ pub trait Dialect: Debug + Any {
 
     /// Returns reserved keywords for projection item prefix operator
     /// e.g. `SELECT CONNECT_BY_ROOT name FROM Tbl2` (Snowflake)
-    fn get_reserved_keywords_for_select_item_operator(&self) -> &[Keyword] {
+    fn get_reserved_keywords_for_select_item(&self) -> &[Keyword] {
         &[]
     }
 

82dff42f8d295d737f821d0a5d40c9c55f61da03
lovasoa <contact@ophir.dev>
2025-04-19 01:16:17 +0200

    xmltable table-valued function
    adds support for xmltable(...)
    
    see https://www.postgresql.org/docs/15/functions-xml.html#FUNCTIONS-XML-PROCESSING
    
    fixes https://github.com/apache/datafusion-sqlparser-rs/issues/1816

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -81,7 +81,8 @@ pub use self::query::{
     TableSampleBucket, TableSampleKind, TableSampleMethod, TableSampleModifier,
     TableSampleQuantity, TableSampleSeed, TableSampleSeedModifier, TableSampleUnit, TableVersion,
     TableWithJoins, Top, TopQuantity, UpdateTableFromKind, ValueTableMode, Values,
-    WildcardAdditionalOptions, With, WithFill,
+    WildcardAdditionalOptions, With, WithFill, XmlPassingArgument, XmlPassingClause,
+    XmlTableColumn, XmlTableColumnOption,
 };
 
 pub use self::trigger::{

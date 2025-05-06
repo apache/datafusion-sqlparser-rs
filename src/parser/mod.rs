eccc1c5458b03ed29b96ab1a943d270f87bca8d6
Andrew Harper <andrew.harper@veracross.com>
2025-05-06 19:38:52 -0400

    Corrected syntax formatting

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -5219,7 +5219,10 @@ impl<'a> Parser<'a> {
                     )?,
                 }
             } else {
-                parser_err!("Expected TABLE keyword after return type", p.peek_token().span.start)?
+                parser_err!(
+                    "Expected TABLE keyword after return type",
+                    p.peek_token().span.start
+                )?
             };
             Ok(DataType::NamedTable(
                 ObjectName(vec![ObjectNamePart::Identifier(return_table_name)]),

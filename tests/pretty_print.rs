963873b37fd6041404008325065a37770bec4933
lovasoa <contact@ophir.dev>
2025-05-10 15:11:56 +0200

    prettify

--- tests/pretty_print.rs
+++ tests/pretty_print.rs
@@ -1,7 +1,7 @@
 use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::Parser;
 
-fn parse_and_format(sql: &str) -> String {
+fn prettify(sql: &str) -> String {
     let ast = Parser::parse_sql(&GenericDialect {}, sql).unwrap();
     format!("{:#}", ast[0])
 }
@@ -9,7 +9,7 @@ fn parse_and_format(sql: &str) -> String {
 #[test]
 fn test_pretty_print_select() {
     assert_eq!(
-        parse_and_format("SELECT a, b, c FROM my_table WHERE x = 1 AND y = 2"),
+        prettify("SELECT a, b, c FROM my_table WHERE x = 1 AND y = 2"),
         r#"
 SELECT
   a,
@@ -27,7 +27,7 @@ WHERE
 #[test]
 fn test_pretty_print_join() {
     assert_eq!(
-        parse_and_format("SELECT a FROM table1 JOIN table2 ON table1.id = table2.id"),
+        prettify("SELECT a FROM table1 JOIN table2 ON table1.id = table2.id"),
         r#"
 SELECT
   a
@@ -42,7 +42,7 @@ FROM
 #[test]
 fn test_pretty_print_subquery() {
     assert_eq!(
-        parse_and_format("SELECT * FROM (SELECT a, b FROM my_table) AS subquery"),
+        prettify("SELECT * FROM (SELECT a, b FROM my_table) AS subquery"),
         r#"
 SELECT
   *
@@ -62,7 +62,7 @@ FROM
 #[test]
 fn test_pretty_print_union() {
     assert_eq!(
-        parse_and_format("SELECT a FROM table1 UNION SELECT b FROM table2"),
+        prettify("SELECT a FROM table1 UNION SELECT b FROM table2"),
         r#"
 SELECT
   a
@@ -81,7 +81,7 @@ FROM
 #[test]
 fn test_pretty_print_group_by() {
     assert_eq!(
-        parse_and_format("SELECT a, COUNT(*) FROM my_table GROUP BY a HAVING COUNT(*) > 1"),
+        prettify("SELECT a, COUNT(*) FROM my_table GROUP BY a HAVING COUNT(*) > 1"),
         r#"
 SELECT
   a,
@@ -100,7 +100,7 @@ HAVING
 #[test]
 fn test_pretty_print_cte() {
     assert_eq!(
-        parse_and_format("WITH cte AS (SELECT a, b FROM my_table) SELECT * FROM cte"),
+        prettify("WITH cte AS (SELECT a, b FROM my_table) SELECT * FROM cte"),
         r#"
 WITH cte AS (
   SELECT
@@ -121,7 +121,7 @@ FROM
 #[test]
 fn test_pretty_print_case_when() {
     assert_eq!(
-        parse_and_format("SELECT CASE WHEN x > 0 THEN 'positive' WHEN x < 0 THEN 'negative' ELSE 'zero' END FROM my_table"),
+        prettify("SELECT CASE WHEN x > 0 THEN 'positive' WHEN x < 0 THEN 'negative' ELSE 'zero' END FROM my_table"),
         r#"
 SELECT
   CASE
@@ -141,7 +141,7 @@ FROM
 #[test]
 fn test_pretty_print_window_function() {
     assert_eq!(
-        parse_and_format("SELECT id, value, ROW_NUMBER() OVER (PARTITION BY category ORDER BY value DESC) as rank FROM my_table"),
+        prettify("SELECT id, value, ROW_NUMBER() OVER (PARTITION BY category ORDER BY value DESC) as rank FROM my_table"),
         r#"
 SELECT
   id,

30202109ac7a6f285ddfb8cbaaffd05d710d82d8
Joshua McQuistan <joshua.mcquistan@feral.io>
2025-07-02 14:26:50 +0100

    Expr::parse_geometric_type should handle updated TypedString

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1708,10 +1708,9 @@ impl<'a> Parser<'a> {
     }
 
     fn parse_geometric_type(&mut self, kind: GeometricTypeKind) -> Result<Expr, ParserError> {
-        let value: Value = self.parse_value()?.value;
         Ok(Expr::TypedString {
             data_type: DataType::GeometricType(kind),
-            value,
+            value: self.parse_value()?,
         })
     }
 

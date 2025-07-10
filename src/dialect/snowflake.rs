cdfd75f3235f13a60aa3528110295eeee84c8713
Yoav Cohen <yoav@satoricyber.com>
2025-07-10 18:08:42 +0200

    Code review comments

--- src/dialect/snowflake.rs
+++ src/dialect/snowflake.rs
@@ -319,8 +319,7 @@ impl Dialect for SnowflakeDialect {
 
             // `FETCH` can be considered an alias as long as it's not followed by `FIRST`` or `NEXT`
             // which would give it a different meanings, for example: `SELECT 1 FETCH FIRST 10 ROWS` - not an alias
-            Keyword::FETCH
-                if parser.peek_keyword(Keyword::FIRST) || parser.peek_keyword(Keyword::NEXT) =>
+            Keyword::FETCH if parser.peek_one_of_keywords(&[Keyword::FIRST, Keyword::NEXT]).is_some() =>
             {
                 false
             }

b258abf3bad2a85c33b8b1d66448091c446ee514
Andrew Harper <andrew.harper@veracross.com>
2025-07-16 14:45:51 -0400

    Expand `require_semicolon_stmt_delimiter` parser option & tests - a corresponding `supports_statements_without_semicolon_delimiter` Dialect trait function - this is optional for SQL Server, so it's set to `true` for that dialect - for the implementation, `RETURN` parsing needs to be tightened up to avoid ambiguity & tests that formerly asserted "end of statement" now maybe need to assert "an SQL statement" - a new `assert_err_parse_statements` splits the dialects based on semicolon requirements & asserts the expected error message accordingly

--- src/dialect/mod.rs
+++ src/dialect/mod.rs
@@ -1122,6 +1122,11 @@ pub trait Dialect: Debug + Any {
     ) -> bool {
         false
     }
+
+    /// Returns true if the dialect supports parsing statements without a semicolon delimiter.
+    fn supports_statements_without_semicolon_delimiter(&self) -> bool {
+        false
+    }
 }
 
 /// This represents the operators for which precedence must be defined

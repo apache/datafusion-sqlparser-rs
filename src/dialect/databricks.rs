9a9fd5291fd070bf9df224b8b56807cf56d35073
Joey Hain <hain.joseph@gmail.com>
2025-09-23 16:59:51 -0700

    Correctly tokenize nested comments in Databricks

--- src/dialect/databricks.rs
+++ src/dialect/databricks.rs
@@ -64,4 +64,9 @@ impl Dialect for DatabricksDialect {
     fn supports_struct_literal(&self) -> bool {
         true
     }
+
+    // https://docs.databricks.com/aws/en/sql/language-manual/sql-ref-syntax-comment
+    fn supports_nested_comments(&self) -> bool {
+        true
+    }
 }

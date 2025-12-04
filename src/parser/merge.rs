0dd12b194d54165ad0b7ddbc5c7b92286924636d
xitep <xitep@users.noreply.github.com>
2025-12-04 11:01:21 +0100

    Update src/parser/merge.rs
    Co-authored-by: Ifeanyi Ubah <ify1992@yahoo.com>

--- src/parser/merge.rs
+++ src/parser/merge.rs
@@ -10,7 +10,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! SQL Parser for MERGE
+//! SQL Parser for a `MERGE` statement
 
 #[cfg(not(feature = "std"))]
 use alloc::{boxed::Box, format, string::ToString, vec, vec::Vec};

adbfc46177aad76b7d67d85fba0dea38a4f777cb
xitep <xitep@users.noreply.github.com>
2025-12-10 12:50:26 +0100

    [Oracle] Lower StringConcat precedence (#2115)

---
+++ tests/sqlparser_oracle.rs
@@ -0,0 +1,105 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+//! Test SQL syntax, specific to [sqlparser::dialect::OracleDialect].
+
+#[cfg(test)]
+use pretty_assertions::assert_eq;
+
+use sqlparser::{
+    ast::{BinaryOperator, Expr, Value, ValueWithSpan},
+    dialect::OracleDialect,
+    tokenizer::Span,
+};
+use test_utils::{expr_from_projection, number, TestedDialects};
+
+mod test_utils;
+
+fn oracle() -> TestedDialects {
+    TestedDialects::new(vec![Box::new(OracleDialect)])
+}
+
+/// Oracle: `||` has a lower precedence than `*` and `/`
+#[test]
+fn muldiv_have_higher_precedence_than_strconcat() {
+    // ...............  A .. B ...... C .. D ...........
+    let sql = "SELECT 3 / 5 || 'asdf' || 7 * 9 FROM dual";
+    let select = oracle().verified_only_select(sql);
+    assert_eq!(1, select.projection.len());
+    assert_eq!(
+        expr_from_projection(&select.projection[0]),
+        // (C || D)
+        &Expr::BinaryOp {
+            // (A || B)
+            left: Box::new(Expr::BinaryOp {
+                // A
+                left: Box::new(Expr::BinaryOp {
+                    left: Box::new(Expr::Value(number("3").into())),
+                    op: BinaryOperator::Divide,
+                    right: Box::new(Expr::Value(number("5").into())),
+                }),
+                op: BinaryOperator::StringConcat,
+                right: Box::new(Expr::Value(ValueWithSpan {
+                    value: Value::SingleQuotedString("asdf".into()),
+                    span: Span::empty(),
+                })),
+            }),
+            op: BinaryOperator::StringConcat,
+            // D
+            right: Box::new(Expr::BinaryOp {
+                left: Box::new(Expr::Value(number("7").into())),
+                op: BinaryOperator::Multiply,
+                right: Box::new(Expr::Value(number("9").into())),
+            }),
+        }
+    );
+}
+
+/// Oracle: `+`, `-`, and `||` have the same precedence and parse from left-to-right
+#[test]
+fn plusminus_have_same_precedence_as_strconcat() {
+    // ................ A .. B .... C .. D ............
+    let sql = "SELECT 3 + 5 || '.3' || 7 - 9 FROM dual";
+    let select = oracle().verified_only_select(sql);
+    assert_eq!(1, select.projection.len());
+    assert_eq!(
+        expr_from_projection(&select.projection[0]),
+        // D
+        &Expr::BinaryOp {
+            left: Box::new(Expr::BinaryOp {
+                // B
+                left: Box::new(Expr::BinaryOp {
+                    // A
+                    left: Box::new(Expr::BinaryOp {
+                        left: Box::new(Expr::Value(number("3").into())),
+                        op: BinaryOperator::Plus,
+                        right: Box::new(Expr::Value(number("5").into())),
+                    }),
+                    op: BinaryOperator::StringConcat,
+                    right: Box::new(Expr::Value(ValueWithSpan {
+                        value: Value::SingleQuotedString(".3".into()),
+                        span: Span::empty(),
+                    })),
+                }),
+                op: BinaryOperator::StringConcat,
+                right: Box::new(Expr::Value(number("7").into())),
+            }),
+            op: BinaryOperator::Minus,
+            right: Box::new(Expr::Value(number("9").into()))
+        }
+    );
+}

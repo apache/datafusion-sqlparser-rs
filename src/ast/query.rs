5e7aca1d330292e0a02d474c103c641f1de236e9
finchxxia <13153363548@163.com>
2026-01-16 13:27:16 +0800

    Update query.rs

--- src/ast/query.rs
+++ src/ast/query.rs
@@ -1242,8 +1242,6 @@ pub enum TableFactor {
         lateral: bool,
         subquery: Box<Query>,
         alias: Option<TableAlias>,
-        /// Optional table sample modifier
-        sample: Option<TableSampleKind>,
     },
     /// `TABLE(<expr>)[ AS <alias> ]`
     TableFunction {
@@ -1924,7 +1922,6 @@ impl fmt::Display for TableFactor {
                 lateral,
                 subquery,
                 alias,
-                sample,
             } => {
                 if *lateral {
                     write!(f, "LATERAL ")?;
@@ -1936,9 +1933,6 @@ impl fmt::Display for TableFactor {
                 f.write_str(")")?;
                 if let Some(alias) = alias {
                     write!(f, " {alias}")?;
-                }
-                if let Some(TableSampleKind::AfterTableAlias(sample)) = sample {
-                    write!(f, " {sample}")?;
                 }
                 Ok(())
             }

3b9b05e6e6e18487e6e6238ff4cecb8528fc90ad
isaacparker0 <128327439+isaacparker0@users.noreply.github.com>
2026-02-02 11:18:44 -0500

    Fix parsing cast operator after parenthesized `DEFAULT` expression (#2168)

--- tests/sqlparser_common.rs
+++ tests/sqlparser_common.rs
@@ -17377,6 +17377,12 @@ fn test_parse_not_null_in_column_options() {
 }
 
 #[test]
+fn test_parse_default_expr_with_operators() {
+    all_dialects().verified_stmt("CREATE TABLE t (c INT DEFAULT (1 + 2) + 3)");
+    all_dialects().verified_stmt("CREATE TABLE t (c INT DEFAULT (1 + 2) + 3 NOT NULL)");
+}
+
+#[test]
 fn test_parse_default_with_collate_column_option() {
     let sql = "CREATE TABLE foo (abc TEXT DEFAULT 'foo' COLLATE 'en_US')";
     let stmt = all_dialects().verified_stmt(sql);

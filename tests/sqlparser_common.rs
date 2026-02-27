d7fefda652f046cf83ed789b96344697bc5aca91
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2026-02-27 19:07:20 +0100

    test(postgres): cover statistics syntax, strictness, and errors

--- tests/sqlparser_common.rs
+++ tests/sqlparser_common.rs
@@ -15218,6 +15218,7 @@ fn parse_comments() {
         ("ROLE", CommentObject::Role),
         ("SCHEMA", CommentObject::Schema),
         ("SEQUENCE", CommentObject::Sequence),
+        ("STATISTICS", CommentObject::Statistics),
         ("TABLE", CommentObject::Table),
         ("TYPE", CommentObject::Type),
         ("USER", CommentObject::User),

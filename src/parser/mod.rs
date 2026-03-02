7779f7ca6fdc2198a38c3bb95e589927647aeccb
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2026-03-02 21:24:38 +0100

    parser: format COPY STDIN payload condition consistently

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -11027,14 +11027,13 @@ impl<'a> Parser<'a> {
         while let Some(opt) = self.maybe_parse(|parser| parser.parse_copy_legacy_option())? {
             legacy_options.push(opt);
         }
-        let values = if matches!(target, CopyTarget::Stdin)
-            && self.peek_token_ref().token != Token::EOF
-        {
-            self.expect_token(&Token::SemiColon)?;
-            self.parse_tsv()
-        } else {
-            vec![]
-        };
+        let values =
+            if matches!(target, CopyTarget::Stdin) && self.peek_token_ref().token != Token::EOF {
+                self.expect_token(&Token::SemiColon)?;
+                self.parse_tsv()
+            } else {
+                vec![]
+            };
         Ok(Statement::Copy {
             source,
             to,

2f7e51ee1671799c2240181eeadc1d99902c4c63
Cora Sutton <cora@sutton.me>
2026-09-12 19:23:57 +0000

    DuckDB: Support SET VARIABLE

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -3279,6 +3279,15 @@ impl Display for FromTable {
 #[cfg_attr(feature = "visitor", derive(Visit, VisitMut))]
 /// Variants for the `SET` family of statements.
 pub enum Set {
+    /// ```sql
+    /// SET VARIABLE variable_name = expression
+    /// ```
+    SetVariable {
+        /// Variable name to assign.
+        variable: ObjectName,
+        /// Value assigned to the variable.
+        value: Expr,
+    },
     /// SQL Standard-style
     /// SET a = 1;
     /// `SET var = value` (standard SQL-style assignment).
@@ -3383,6 +3392,9 @@ pub enum Set {
 impl Display for Set {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
+            Self::SetVariable { variable, value } => {
+                write!(f, "SET VARIABLE {variable} = {value}")
+            }
             Self::ParenthesizedAssignments { variables, values } => write!(
                 f,
                 "SET ({}) = ({})",

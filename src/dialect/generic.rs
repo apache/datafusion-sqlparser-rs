d3f95dab8e3bda01358631acccef6e5a6bd6b59c
Cora Sutton <cora@sutton.me>
2026-09-12 17:47:28 +0000

    DuckDB: Support window-frame EXCLUDE

--- src/dialect/generic.rs
+++ src/dialect/generic.rs
@@ -45,6 +45,10 @@ impl Dialect for GenericDialect {
         true
     }
 
+    fn supports_window_frame_exclusion(&self) -> bool {
+        true
+    }
+
     fn supports_partition_by_after_order_by(&self) -> bool {
         true
     }

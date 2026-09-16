c4c453bfb5d8a7c1ef7a70c0d4c092763ceeb09f
Ben Herzberg <ben@satoricyber.com>
2026-09-16 11:02:15 +0300

    Address review comments: broaden TABLE command test to pg_and_generic, add ONLY/* coverage for AS TABLE

--- tests/sqlparser_postgres.rs
+++ tests/sqlparser_postgres.rs
@@ -9956,7 +9956,7 @@ fn parse_insert_by_name_keywords_as_table_and_alias() {
 
 #[test]
 fn parse_table_command() {
-    pg().verified_stmt("TABLE customers ORDER BY contact_name LIMIT 1");
-    pg().verified_stmt("TABLE ONLY customers");
-    pg().verified_stmt("TABLE customers *");
+    pg_and_generic().verified_stmt("TABLE customers ORDER BY contact_name LIMIT 1");
+    pg_and_generic().verified_stmt("TABLE ONLY customers");
+    pg_and_generic().verified_stmt("TABLE customers *");
 }

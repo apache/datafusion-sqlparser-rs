9de1ad17b137e5e43493057974ca98e2e1ad93f1
Dmitri B. <s5dsn-eqee.dev@proton.me>
2026-09-23 19:18:40 +0000

    ClickHouse: Support FIRST | AFTER column position in ALTER TABLE (#2535)

--- src/dialect/generic.rs
+++ src/dialect/generic.rs
@@ -320,4 +320,8 @@ impl Dialect for GenericDialect {
     fn supports_aliased_function_args(&self) -> bool {
         true
     }
+
+    fn supports_alter_column_position(&self) -> bool {
+        true
+    }
 }
